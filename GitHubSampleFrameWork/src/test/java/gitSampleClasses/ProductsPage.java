dfc5281cf1cece5e7956b624747d1b2a18b1321a
anjal <anjal@ANCHAL.mshome.net>
2025-03-11 14:47:12 +0530

    Added products Page

---
+++ GitHubSampleFrameWork/src/test/java/gitSampleClasses/ProductsPage.java
@@ -0,0 +1,24 @@
+package gitSampleClasses;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ProductsPage {
+
+	public static void main(String[] args) {
+		WebDriver driver = new  ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://localhost:8888/");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+		driver.findElement(By.linkText("Products")).click();
+		String ProductsPage = driver.getTitle();
+		System.out.println(ProductsPage);
+	}
+
+}
